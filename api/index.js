8b7f3d33b53d22752061c503a5ed58a4cd9dd58e
Kumar Shiavm <kum.sh1vam315@gmail.com>
2024-08-01 22:19:04 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(
+    "mongodb+srv://kumar:shivam@mern.z89sxe3.mongodb.net/mern-authretryWrites=true&w=majority&appName=mern"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
