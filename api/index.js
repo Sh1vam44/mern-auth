825318a5d2bc7b08b2e2b863977bb30ad831177a
Kumar Shiavm <kum.sh1vam315@gmail.com>
2024-08-03 03:33:00 +0530

    create get route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/user.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -20,3 +20,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
+
+app.use("/api/user", userRoutes);
