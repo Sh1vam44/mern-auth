49a81e3f4d7499e09b2da6b48e641bffee00749d
Kumar Shiavm <kum.sh1vam315@gmail.com>
2024-08-03 15:02:55 +0530

    complete sign up page ui

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import Singin from "./Pages/Singin";
