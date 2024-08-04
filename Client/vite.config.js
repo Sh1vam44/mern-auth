abf0f8fa473bfa3ff14f7b4e18d148f969468e1b
Kumar Shiavm <kum.sh1vam315@gmail.com>
2024-08-04 18:48:38 +0530

    complete sign up page

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, sortUserPlugins } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { plugin } from "mongoose";
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    // vite config
-    define: {
-      VITE_MONGO: JSON.stringify(env.VITE_MONGO),
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
     },
-  };
+  },
 });
